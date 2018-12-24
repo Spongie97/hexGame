f81a3b00f248f64f733eab478905483bb6b3f78d
Spongie97 <emily860922@gmail.com>
2018-12-24 09:27:45 +0800

    first commit

---
+++ hexGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hexGame
+//
+//  Created by Spongie on 2016/6/19.
+//  Copyright © 2016年 Spongie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
